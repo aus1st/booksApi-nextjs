22298b3c719087158729f01a5f95a118958981b6
Ahmed Uddin Siddiqui <aus1st@gmail.com>
2023-03-30 15:32:16 +0500

    place order

--- booksapinextjs/app/shared/placeOrder.tsx
+++ booksapinextjs/app/shared/placeOrder.tsx
@@ -5,32 +5,31 @@ import { newOrder } from '../models/client';
 
 
 const token = '6d2344f30a967651bb18967444ddfb8a0a24f53e21e6b0f27cde101c4cfb4d23'
+const baseUrl = 'https://simple-books-api.glitch.me/orders';
 
-
-async function PlaceNewOrder(order:newOrder){
-  const options:any = {  
+async function PlaceNewOrder(order: newOrder) {
+  const options: any = {
     method: 'POST',
-    mode:'no-cors',
+    mode: 'no-cors',
     headers: {
-     
       'Authorization': `Bearer ${token}`,
     },
-    body: JSON.stringify({order})
+    body: JSON.stringify({ order })
   }
-const res = await fetch('https://simple-books-api.glitch.me/orders',options);
-if(res.ok) {
- return await res.json();
-} else {
-  console.log(res);
-}
+  const res = await fetch(baseUrl, options);
+  if (res.ok) {
+    return await res.json();
+  } else {
+    console.log(res);
+  }
 }
 
-const PlaceOrder: NextPage<newOrder> = (props)=>{
-  const {bookId,customerName } = props;
+const PlaceOrder: NextPage<newOrder> = (props) => {
+  const { bookId, customerName } = props;
   return (
     <div>
-        <button             className={`bg-blue-600 text-white py-1 px-4 rounded-md`}
- type="button" onClick={async ()=> await PlaceNewOrder({bookId,customerName})}>Place Order</button>
+      <button className={`bg-blue-600 text-white py-1 px-4 rounded-md`}
+        type="button" onClick={async () => await PlaceNewOrder({ bookId, customerName })}>Place Order</button>
     </div>
   )
 }
